5c0dd8e37f7e1bea172a51f5a9be2e8e85808301
rivpii <oli.xiwen.zhao@gmail.com>
2026-07-23 09:53:19 -0400

    Tried to edit style.css. I am not seeing the changes in the host. I struggle with Docker. This is driving  me crazy

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,8 @@
 ﻿Write-Host "==> Deploying plugin files to Moodle container..." -ForegroundColor Green
-docker cp ./local_securecoursehub/. secure-course-hub-lab-moodle-1:/opt/bitnami/moodle/local/securecoursehub
+docker cp ./local_securecoursehub secure-course-hub-lab-moodle-1:/opt/bitnami/moodle/local/securecoursehub
 
 Write-Host "==> Running Moodle database upgrade..." -ForegroundColor Green
-docker exec -u root secure-course-hub-lab-moodle-1 php /opt/bitnami/moodle/admin/cli/upgrade.php --non-interactive
+docker exec secure-course-hub-lab-moodle-1 php /opt/bitnami/moodle/admin/cli/upgrade.php --non-interactive
 
-Write-Host "==> Purging Moodle caches..." -ForegroundColor Green
-docker exec -u root secure-course-hub-lab-moodle-1 php /opt/bitnami/moodle/admin/cli/purge_caches.php
-
-Write-Host "==> Fixing container file permissions..." -ForegroundColor Yellow
-docker exec -u root secure-course-hub-lab-moodle-1 bash -c "chmod -R 777 /opt/bitnami/moodle /bitnami"
-
-Write-Host "++ Deployment & Upgrade Complete ++" -ForegroundColor Cyan
+Write-Host "++ Deployment Complete ++" -ForegroundColor Cyan
 Write-Host "Access your site at: http://127.0.0.1:8080" -ForegroundColor Yellow
